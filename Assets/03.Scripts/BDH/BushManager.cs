8f2b21edf4614e7ec6539f641a62e08aba2f33e4
ByunDongHwan <63548551+byundonghwan@users.noreply.github.com>
2023-08-04 22:02:56 +0900

    2023.08.04
    GUI 로비 메인 화면 및 Loding 씬 작업.

---
+++ Assets/03.Scripts/BDH/BushManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 환경 : 부쉬 기능 
+// 1. 
+
+public class BushManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
