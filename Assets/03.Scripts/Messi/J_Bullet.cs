0ce4304913540b2ae5dba54b0ad277cace3ef6d3
JCURVEs <jaehee2173@gmail.com>
2023-08-06 22:24:58 +0900

    shelly move & fire

--- Assets/03.Scripts/Messi/J_Bullet.cs
+++ Assets/03.Scripts/Messi/J_Bullet.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 
 public class J_Bullet : MonoBehaviour
 {
-    public float speed = 10f;
+    //public float speed = 10f;
+
+    [SerializeField]
+    shellyFire SF;
+
+    Vector3 bulletEndDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SF = GameObject.Find("AttackTrail").GetComponent<shellyFire>(); 
+        bulletEndDistance = transform.position + transform.forward * SF.TrailDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.forward * speed * Time.deltaTime;
+        if(transform.position == bulletEndDistance)
+        {
+            //
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject );
+        }
+        //transform.position += Vector3.forward * speed * Time.deltaTime;
     }
 }
--- Assets/03.Scripts/Messi/ShellyMove.cs
+++ Assets/03.Scripts/Messi/ShellyMove.cs
@@ -6,6 +6,14 @@ public class ShellyMove : MonoBehaviour
 {
     [SerializeField]
     public float speed = 5f;
+    [SerializeField]
+    bl_Joystick joystick;
+
+    [SerializeField]
+    Transform PlayerSprite;
+
+
+    bool Movement;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +23,35 @@ public class ShellyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");   
-        float v = Input.GetAxis("Vertical");
-        Vector3 dir = h * Vector3.right + v * Vector3.forward;
+        PlayerSprite.position = new Vector3(joystick.Horizontal + transform.position.x,-1.54f,joystick.Vertical + transform.position.z);
+
+        transform.LookAt(new Vector3(PlayerSprite.position.x, 0, PlayerSprite.position.z));
+        //float h = Input.GetAxis ("Horizontal");   
+        //float v = Input.GetAxis("Vertical");
+        //Vector3 dir = h * Vector3.right + v * Vector3.forward;
         //Vector3 dir = new Vector3(0, h, 0);
-        dir.Normalize();
+        //dir.Normalize();
 
-        transform.position += dir * speed * Time.deltaTime; 
+        //transform.position += dir * speed * Time.deltaTime; 
+        transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);   
 
-        
+        if(joystick.Horizontal > 0 || joystick.Horizontal < 0 || joystick.Vertical >0 || joystick.Vertical < 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+
+            //애니메이션 walks true
+
+
+            //무브먼트 실행
+            Movement = true;
+        }
+        else if (Movement == true)
+        {
+            //애니메이션 walk,false
+            Movement = false;
+        }
+
+
 
     }
 }
