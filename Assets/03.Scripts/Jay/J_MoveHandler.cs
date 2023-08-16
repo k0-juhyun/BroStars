b6cd8af9a3d8145c3ef3e384997e9a6f971f4270
JCURVEs <jaehee2173@gmail.com>
2023-08-16 18:01:56 +0900

    Jay_Shelly Dust 이펙트

--- Assets/03.Scripts/Jay/J_MoveHandler.cs
+++ Assets/03.Scripts/Jay/J_MoveHandler.cs
@@ -22,13 +22,17 @@ public class J_MoveHandler : MonoBehaviour
     public float moveSpeed = 5f;
     #endregion
 
+    [SerializeField]
+    [Header("Particle System")]
+    private ParticleSystem[] dustPrefab;
+
     Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         lookPoint = transform.GetChild(0).gameObject.GetComponent<Transform>();
-        animatorHandler = GetComponent<J_AnimatorHandler>();    
+        //animatorHandler = GetComponent<J_AnimatorHandler>();    
         animator = GetComponent<Animator>();
     }
 
@@ -36,67 +40,35 @@ public class J_MoveHandler : MonoBehaviour
     public void HandleMovement()
     {
         //움직임 컨트롤러 값
-        if(joystick.Horizontal > 0  || joystick.Horizontal <0 || joystick.Vertical >0 || joystick.Vertical <0)
+        if (joystick.Horizontal > 0 || joystick.Horizontal < 0 || joystick.Vertical > 0 || joystick.Vertical < 0)
         {
             lookPoint.position = new Vector3(joystick.Horizontal + transform.position.x, 6.1f, joystick.Vertical + transform.position.z);
             transform.LookAt(new Vector3(lookPoint.position.x, 6.1f, lookPoint.position.z));
-            transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);   
-            transform.Translate(Vector3.forward * moveSpeed *Time.fixedDeltaTime);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            transform.Translate(Vector3.forward * moveSpeed * Time.fixedDeltaTime);
             //애니메이터
             animator.Play("Walking");
             //bool 움직임
             moveFlag = true;
         }
-        else if( moveFlag == true)
+        else if (moveFlag == true)
         {
             moveFlag = false;
         }
-        //PlayerSprite.position = new Vector3(joystick.Horizontal + transform.position.x,6.1f,joystick.Vertical + transform.position.z);
 
-        //float h = Input.GetAxis ("Horizontal");   
-        //float v = Input.GetAxis("Vertical");
-        //Vector3 dir = h * Vector3.right + v * Vector3.forward;
-        //Vector3 dir = new Vector3(0, h, 0);
-        //dir.Normalize();
+    }
+    public void HandleLeftDust()
+    {
+        ParticleSystem particles = Instantiate(dustPrefab[0], transform.position, Quaternion.identity);
+        particles.Play();
 
-        //transform.position += dir * speed * Time.deltaTime; 
+        Destroy(particles.gameObject, particles.main.duration);
+    }
+    public void HandleRightDust()
+    {
+        ParticleSystem particles = Instantiate(dustPrefab[1], transform.position, Quaternion.identity);
+        particles.Play();
 
-
-
-        //if(joystick.Horizontal > 0 || joystick.Horizontal < 0 || joystick.Vertical > 0 || joystick.Vertical < 0)
-        //{
-        //    //애니메이션 walks true
-
-
-        //    //무브먼트 실행
-        //    Movement = true;
-        //}
-        //else if (Movement == true)
-        //{
-        //    //애니메이션 walk,false
-        //    Movement = false;
-        //}
-
-        //if (joystick.Horizontal > 0 || joystick.Horizontal < 0 || joystick.Vertical > 0 || joystick.Vertical < 0)
-        //{
-        //    lookPoint.position = new Vector3(joystick.Horizontal + transform.position.x, 6.1f, joystick.Vertical + transform.position.z);
-
-        //    transform.LookAt(new Vector3(lookPoint.position.x, 6.1f, lookPoint.position.z));
-
-        //    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-
-        //    transform.Translate(Vector3.forward * Time.fixedDeltaTime * moveSpeed);
-
-        //    animatorHandler.playerTargetAnim("Walking");
-
-        //    moveFlag = true;
-        //}
-
-        //else if (moveFlag == true)
-        //{
-        //    moveFlag = false;
-        //}
-
-
+        Destroy(particles.gameObject, particles.main.duration);
     }
 }
