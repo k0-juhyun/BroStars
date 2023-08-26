3b344e304970a919cdd3f4c804859fd4f2ec5a74
k0-juhyun <79074052+k0-juhyun@users.noreply.github.com>
2023-08-26 23:42:24 +0900

    ㅁㄴㅇㄹ
    ㅁㄴㅇㄹ

--- Assets/03.Scripts/Jay/ShellyManager.cs
+++ Assets/03.Scripts/Jay/ShellyManager.cs
@@ -6,14 +6,9 @@ using Photon.Pun;
 public class ShellyManager : MonoBehaviourPun
 {
     MoveHandler moveHandler;
-    //J_attackHandler attackHandler;
     ShellyAttackHandler attackHandler;
     HpHandler hpHandler;
     BushManager bushManager;
-
-    // Start is called before the first frame update
-
-    public int gem;
 
     void Awake()
     {
@@ -26,9 +21,8 @@ public class ShellyManager : MonoBehaviourPun
 
         hpHandler = GetComponent<HpHandler>();
         bushManager = GetComponent <BushManager>();
-       
     }
-    // Update is called once per frame
+
     private void FixedUpdate()
     {
         if (photonView.IsMine == false)
@@ -37,20 +31,7 @@ public class ShellyManager : MonoBehaviourPun
         moveHandler.HandleMovement();
         attackHandler.HandleNormalAttack();
         attackHandler.HandleUltimateAttack();
-        //attackHandler.UpdateFire();
+
         hpHandler.RegenerateHpInBush();
     }
-
-    //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    //{
-    //    if (stream.IsWriting)
-    //    {
-    //        stream.SendNext(hpHandler.curHp);
-    //    }
-    //    else
-    //    {
-    //        float newHp = (float)stream.ReceiveNext();
-    //        hpHandler.UpdateHp(newHp);
-    //    }
-    //}
 }
